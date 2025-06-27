ce80b47964c5ca4f94fb1e0459c876d37ec39f13
Arunav Singh <arunavsingh999@gmail.com>
2025-06-27 18:06:01 +0530

    completed radial design of mobile nav

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -7,7 +7,9 @@ import "./header.css";
 
 const Header = () => {
   const [linkBg, setLinkBg] = useState(false);
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
 
+  // For handling scroll effect of header
   useEffect(() => {
     const handleScroll = () => {
       const navArea = document.getElementById("navArea");
@@ -36,7 +38,46 @@ const Header = () => {
         </h2>
 
         {/* NAVIGATION */}
-        <nav>
+        {/* style={{
+              background: "radial-gradient(circle at center, #8dcac1, #53aea0)",
+            }} */}
+        {/* Mobile Nav toggle button - Visible below lg */}
+        <div className="relative lg:hidden h-13 w-13 rounded-[50%] flex items-center justify-center shadow-nav">
+          <button
+            onClick={() => setIsMobileNavOpen((prev) => !prev)}
+            className="cursor-pointer"
+          >
+            <div className="absolute rounded-[50%] h-full w-full top-0 right-0 z-30 bg-white">
+              MENU
+            </div>
+            <div
+              className={`h-11 w-11 rounded-[50%] ${
+                isMobileNavOpen ? "scale-[80]" : "scale-[0]"
+              } transition-all duration-800 ease-in-out`}
+              style={{
+                background:
+                  "radial-gradient(circle at center, rgba(141, 202, 193, 0.3), rgba(83, 174, 160, 0.3))",
+                  backdropFilter: "blur(0.05px)",
+              }}
+            ></div>
+          </button>
+        </div>
+
+        {/* <div className="fixed top-22 right-22 h-16 w-16 rounded-[50%] overflow-hidden">
+          <input type="checkbox" name="nav-small" id="nav-toggle" />
+
+          <label htmlFor="nav-toggle">MENU</label>
+
+          <div
+            className="absolute top-0 h-full w-full"
+            
+          >
+            &nbsp;
+          </div>
+        </div> */}
+
+        {/* NAVIGATION-FOR-LARGER-DEVICES */}
+        <nav className="hidden lg:block">
           <ul className="flex items-center gap-4">
             <li>
               <Links to="/about" bg={linkBg}>
