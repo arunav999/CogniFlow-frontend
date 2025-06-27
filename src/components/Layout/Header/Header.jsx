14f3147395999cab158dd0baa2a6d6a90c318b2e
Arunav Singh <arunavsingh999@gmail.com>
2025-06-27 19:33:57 +0530

    nav done for mobile devices

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -38,16 +38,16 @@ const Header = () => {
         </h2>
 
         {/* NAVIGATION */}
-        {/* style={{
-              background: "radial-gradient(circle at center, #8dcac1, #53aea0)",
-            }} */}
-        {/* Mobile Nav toggle button - Visible below lg */}
         <div className="relative lg:hidden h-13 w-13 rounded-[50%] flex items-center justify-center shadow-nav">
           <button
             onClick={() => setIsMobileNavOpen((prev) => !prev)}
             className="cursor-pointer"
           >
-            <div className="absolute rounded-[50%] h-full w-full top-0 right-0 z-30 bg-white">
+            <div
+              className={`absolute rounded-[50%] h-full w-full top-0 right-0 z-30 ${
+                linkBg ? "bg-light-bg-body" : "bg-light-bg-card"
+              }`}
+            >
               MENU
             </div>
             <div
@@ -57,49 +57,43 @@ const Header = () => {
               style={{
                 background:
                   "radial-gradient(circle at center, rgba(141, 202, 193, 0.3), rgba(83, 174, 160, 0.3))",
-                  backdropFilter: "blur(0.05px)",
+                backdropFilter: "blur(0.05px)",
               }}
             ></div>
           </button>
         </div>
 
-        {/* <div className="fixed top-22 right-22 h-16 w-16 rounded-[50%] overflow-hidden">
-          <input type="checkbox" name="nav-small" id="nav-toggle" />
-
-          <label htmlFor="nav-toggle">MENU</label>
-
-          <div
-            className="absolute top-0 h-full w-full"
-            
-          >
-            &nbsp;
-          </div>
-        </div> */}
-
         {/* NAVIGATION-FOR-LARGER-DEVICES */}
-        <nav className="hidden lg:block">
-          <ul className="flex items-center gap-4">
-            <li>
+        {/* top-[70%] left-[50%] transform -translate-x-[50%] translate-y-[70%] */}
+        <nav
+          className={`nav ${
+            isMobileNavOpen
+              ? "nav-open"
+              : ""
+          } `}
+        >
+          <ul className="flex flex-col lg:flex-row items-center gap-8 lg:gap-4">
+            <li onClick={() => setIsMobileNavOpen(false)}>
               <Links to="/about" bg={linkBg}>
                 About
               </Links>
             </li>
-            <li>
+            <li onClick={() => setIsMobileNavOpen(false)}>
               <Links to="/features" bg={linkBg}>
                 Features
               </Links>
             </li>
-            <li>
+            <li onClick={() => setIsMobileNavOpen(false)}>
               <Links to="/demo" bg={linkBg}>
                 Demo
               </Links>
             </li>
-            <li>
+            <li onClick={() => setIsMobileNavOpen(false)}>
               <Links to="/pricing" bg={linkBg}>
                 Pricing
               </Links>
             </li>
-            <li>
+            <li onClick={() => setIsMobileNavOpen(false)}>
               <Button secondary disabled={false}>
                 Get Started
               </Button>
--- src/components/Reusable/Links/Links.jsx
+++ src/components/Reusable/Links/Links.jsx
@@ -11,7 +11,7 @@ const Links = ({ children, to, bg }) => {
             isActive
               ? "bg-light-text-link text-white bg-[linear-gradient(240deg,_transparent_0%,_transparent_50%,_var(--color-light-ui-secondary)_50%)] bg-[length:230%] hover:bg-[position:100%] hover:text-white"
               : `${bg ? "bg-light-bg-body" : "bg-light-bg-card"}`
-          } rounded-2xl transition-all duration-300 bg-[linear-gradient(120deg,_transparent_0%,_transparent_50%,_var(--color-light-ui-primary)_50%)] bg-[length:230%] hover:bg-[position:100%] hover:text-white `
+          } rounded-2xl transition-all duration-300 bg-[linear-gradient(120deg,_transparent_0%,_transparent_50%,_var(--color-light-ui-primary)_50%)] bg-[length:230%] hover:bg-[position:100%] hover:text-white`
         }
       >
         {children}
