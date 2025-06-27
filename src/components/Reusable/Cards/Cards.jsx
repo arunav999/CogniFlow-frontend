d904971ab384a615c96c17acc34be31e35c42065
Arunav Singh <arunavsingh999@gmail.com>
2025-06-27 11:03:07 +0530

    cards design completed

--- src/components/Reusable/Cards/Cards.jsx
+++ src/components/Reusable/Cards/Cards.jsx
@@ -14,24 +14,38 @@ const Cards = ({
   // w-full aspect-[3/4] max-w-[clamp(18rem, 30vw, 30rem)]
   return (
     <div className="relative w-80 h-120 perspective-[1000px] cursor-pointer">
-      <div className="w-full h-full transition-transform duration-700 [transform-style:preserve-3d] hover:-rotate-y-180 rounded-2xl shadow-card">
+      <div className="w-full h-full transition-transform ease-in duration-500 [transform-style:preserve-3d] hover:-rotate-y-180 rounded-2xl shadow-card">
         {/* Front Side */}
-        <div
-          className="absolute w-full h-full bg-white backface-hidden rounded-2xl flex  flex-col overflow-hidden"
-          
-        >
-          <div className="font-heading uppercase text-lg  text-white w-full h-[40%] flex items-center justify-center" style={{ backgroundImage: front }}>
-            <p className="border py-2.5 px-6">{plan}</p>
+        <div className="absolute w-full h-full bg-white backface-hidden rounded-2xl flex  flex-col overflow-hidden">
+          <div
+            className="relative font-heading uppercase text-lg  text-white w-full h-[40%] clip-bottom"
+            style={{ backgroundImage: front }}
+          >
+            <h4 className="py-2.5 px-6 absolute bottom-15 right-10">
+              <span
+                className="py-2 px-4 shadow-card"
+                style={{ backgroundImage: front }}
+              >
+                {plan}
+              </span>
+            </h4>
           </div>
-          <div className="font-body border">
-            {details.map((point, index) => (
-              <p key={index} className="flex items-center gap-3">
-                <span>
+          <div className="font-body m-4">
+            <ul className="flex flex-col text-sm gap-4">
+              {details.map((point, index) => (
+                <>
+                  <li
+                    key={index}
+                    className="flex items-center gap-4 text-light-text-secondary"
+                  >
+                    <span>
                   <TiTick />
-                </span>
-                <span>{point}</span>
-              </p>
-            ))}
+                  </span>
+                    <span>{point}</span>
+                  </li>
+                </>
+              ))}
+            </ul>
           </div>
         </div>
 
