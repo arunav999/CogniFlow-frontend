f23d6defb1538766894408848152552315d8140e
Arunav Singh <arunavsingh999@gmail.com>
2025-06-27 11:43:11 +0530

    sections headings completed

--- src/components/Main/Pricing/PricingSection.jsx
+++ src/components/Main/Pricing/PricingSection.jsx
@@ -5,19 +5,34 @@ import CARD_DATA from "../../../utils/data";
 const PricingSection = () => {
   return (
     <>
-      <section className="flex xs:flex-col lg:flex-row items-center justify-evenly gap-10 py-12 px-24">
-        {CARD_DATA.map((item) => (
-          <Cards
-            key={item.id}
-            plan={item.plan}
-            price={item.price}
-            details={item.details}
-            button={item.button}
-            front={item.front}
-            gradient={item.gradient}
-            secondary={item.secondary}
-          />
-        ))}
+      <section className="section">
+        <div className="text-center mb-16">
+          <h2 className="heading">
+            Simple Plans, Built for Flow
+          </h2>
+          <h4 className="sub-heading">
+            Whether you're solo or scaling,{" "}
+            <span className="font-logo xs:text-2xl sm:text-3xl font-bold">
+              Cogniflow
+            </span>{" "}
+            has a plan that fits your style.
+          </h4>
+        </div>
+
+        <div className="flex xs:flex-col lg:flex-row items-center justify-evenly gap-10 ">
+          {CARD_DATA.map((item) => (
+            <Cards
+              key={item.id}
+              plan={item.plan}
+              price={item.price}
+              details={item.details}
+              button={item.button}
+              front={item.front}
+              gradient={item.gradient}
+              secondary={item.secondary}
+            />
+          ))}
+        </div>
       </section>
     </>
   );
