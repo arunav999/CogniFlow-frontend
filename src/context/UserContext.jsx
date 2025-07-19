1f27bf571752dd84eee0b4c95e5bcf6660648582
Arunav Singh <arunavsingh999@gmail.com>
2025-07-19 13:30:41 +0530

    fixed loading issue

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -4,9 +4,6 @@ import { useState, useEffect, createContext } from "react";
 // React-Router
 import { useNavigate, useLocation } from "react-router-dom";
 
-// Constants
-import { ROLES } from "../utils/roles/roles";
-
 // Get user info service
 import { getUserInfo } from "../services/Auth/authService";
 
@@ -31,11 +28,13 @@ export const UserProvider = ({ children }) => {
 
   // Run on first render
   useEffect(() => {
-
     const checkAuth = async () => {
       try {
         // Getting user
         const res = await getUserInfo();
+
+        // Get role
+        const redirect = res.redirect;
 
         // Setting user
         setStatus((prev) => ({
@@ -43,38 +42,32 @@ export const UserProvider = ({ children }) => {
           user: res.user,
         }));
 
-        // Get role
-        const userRole = res.user.role;
-        const redirect =
-          userRole === ROLES.ADMIN
-            ? "/admin"
-            : userRole === ROLES.MANAGER
-            ? "/manager"
-            : "/developer";
-
-        // If logged in and already on "/", redirect to dashboard
-        if (location.pathname === "/" || location.pathname === "/auth")
+        // If logged in and on public route then redirect to dashboard
+        if (location.pathname === "/" || location.pathname === "/auth") {
           navigate(redirect);
+        }
+
+        // Set loading
+        setStatus((prev) => ({
+          ...prev,
+          loading: false,
+        }));
       } catch (error) {
         // Retry after delay (for refresh token)
         await new Promise((resolve) => setTimeout(resolve, 2000)); // Retry in 2s
 
         try {
           const retryRes = await getUserInfo();
+          console.log(retryRes);
+
+          // Get role after retry
+
+          const retryRedirect = retryRes.redirect;
 
           setStatus((prev) => ({
             ...prev,
             user: retryRes.user,
           }));
-
-          // Get role after retry
-          const retryUserRole = retryRes.user.role;
-          const retryRedirect =
-            retryUserRole === ROLES.ADMIN
-              ? "/admin"
-              : retryUserRole === ROLES.MANAGER
-              ? "/manager"
-              : "/developer";
 
           // If login via refresh token
           if (location.pathname === "/" || location.pathname === "/auth")
